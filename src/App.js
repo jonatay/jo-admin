c170972d8bd4d32cbc66041e9297dfed2bdfeb95
jonatay <jono.stretch@gmail.com>
2021-06-27 08:57:16 +0200

    initial version

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Topbar from "./components/topbar/Topbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Topbar />
+      <p>Whatever</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/topbar/Topbar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  AssignmentTurnedInTwoTone,
+  ChatBubbleTwoTone,
+  NotificationsNoneTwoTone,
+  SettingsApplicationsTwoTone,
+  AccountCircleTwoTone,
+} from "@material-ui/icons";
+
+import "./topbar.css";
+export default function Topbar() {
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">joAdmin</span>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <AssignmentTurnedInTwoTone />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <ChatBubbleTwoTone />
+            <span className="topIconBadge">6</span>
+          </div>
+          <div className="topbarIconContainer">
+            <NotificationsNoneTwoTone />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <AccountCircleTwoTone />
+            <span className="topIconBadge">3</span>
+          </div>
+          <div className="topbarIconContainer">
+            <SettingsApplicationsTwoTone />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
